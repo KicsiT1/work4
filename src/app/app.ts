4cfc58bcc63c4b1e4c72aba7d8ac360d69946809
Kicsi.T1 <totin.2citromail.hu@gmail.com>
2025-12-16 20:47:23 +0100

    Font Awesome icons are working fine now.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FontAwesomeModule,RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App { }
+export class App {}
