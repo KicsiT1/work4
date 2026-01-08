c13bc9f781f7322f0c0141f815384d5abd436975
Kicsi.T1 <totin.2citromail.hu@gmail.com>
2026-01-08 20:22:44 +0100

    I added the dynamically changing carousel to the project, it is implemented in the current offer component.

--- src/app/components/current-offer/current-offer.ts
+++ src/app/components/current-offer/current-offer.ts
@@ -5,6 +5,14 @@ import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {IconDefinition,} from '@fortawesome/fontawesome-svg-core';
 // The different icons I use for the hero right side 
 import {faAngleLeft,faAngleRight} from '@fortawesome/free-solid-svg-icons';
+interface ProductInfo 
+{
+  ProductImg: string;
+  SaleNumber: string;
+  DiscountValue:string;
+  SaleSeason: string;
+}
+
 @Component({
   selector: 'app-current-offer',
   imports: [FontAwesomeModule],
@@ -12,10 +20,29 @@ import {faAngleLeft,faAngleRight} from '@fortawesome/free-solid-svg-icons';
   styleUrl: './current-offer.scss',
 })
 export class CurrentOffer {
+  NextIndex:number=0;
+  constructor() 
+  {
+    this.NextProduct(0);
+  }
+
   faAngleLeft:IconDefinition=faAngleLeft;
   faAngleRight:IconDefinition=faAngleRight;
+
   Product1:string='/public/assets/images/currentOffer/p1.png';
   Product2:string='/public/assets/images/currentOffer/p2.png';
   Product3:string='/public/assets/images/currentOffer/p3.png';
+
+  ProductInfoItems: ProductInfo [] = 
+  [
+    {ProductImg:this.Product1,SaleNumber:"01",SaleSeason:"Spring sale",DiscountValue:"30%"},
+    {ProductImg:this.Product2,SaleNumber:"02",SaleSeason:"Spring sale",DiscountValue:"20%"},
+    {ProductImg:this.Product3,SaleNumber:"03",SaleSeason:"Spring sale",DiscountValue:"50%"},
+  ];
+  
+  NextProduct(NewIndex:number)
+  {
+    this.NextIndex=NewIndex;
+  }
 
 }
