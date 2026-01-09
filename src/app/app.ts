bc4aa4907eaafca03c620c57048ea400671c2fcf
Kicsi.T1 <totin.2citromail.hu@gmail.com>
2026-01-09 19:35:04 +0100

    I added the cards to the next part of the page, but I still need to work on the cards a little bit.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './components/nav-bar/nav-bar';
 import { HeroSection } from './components/hero-section/hero-section';
-import {CurrentOffer} from './components/current-offer/current-offer';
+import { CurrentOffer } from './components/current-offer/current-offer';
+import{ NewArrivals } from'./components/new-arrivals/new-arrivals'
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavBar,HeroSection,CurrentOffer],
+  imports: [RouterOutlet,NavBar,HeroSection,CurrentOffer,NewArrivals],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
