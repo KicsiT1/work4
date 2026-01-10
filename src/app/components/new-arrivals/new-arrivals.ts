796dd4b211a52e90d2754f2050c53dc13fe742b7
Kicsi.T1 <totin.2citromail.hu@gmail.com>
2026-01-10 15:24:09 +0100

    The New Arrivals section has been added to the project ONLY statically for now.

--- src/app/components/new-arrivals/new-arrivals.ts
+++ src/app/components/new-arrivals/new-arrivals.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+// This makes it safe to use icons (special type for icons)
+import {IconDefinition} from '@fortawesome/fontawesome-svg-core';
+// The different icons I use for the menu
+import {faStar} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-new-arrivals',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './new-arrivals.html',
   styleUrl: './new-arrivals.scss',
 })
 export class NewArrivals {
-
+  faStar:IconDefinition=faStar;
 }
