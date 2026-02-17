c7c425a2b852a97fb568827ba3952966c569b994
Kicsi.T1 <totin.2citromail.hu@gmail.com>
2026-02-17 20:23:14 +0100

    I made the shop filter UI

--- src/app/components/shop/shop.ts
+++ src/app/components/shop/shop.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faAngleUp,faAngleDown} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-Shop',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './shop.html',
-  styleUrl: './shop.css',
+  styleUrl: './shop.scss',
 })
 export class Shop {
-
+    faAngleUp:IconDefinition=faAngleUp;
+    faAngleDown:IconDefinition=faAngleDown;
 }
