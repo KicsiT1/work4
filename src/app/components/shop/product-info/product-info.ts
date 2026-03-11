189729f08e19025c0d199694f87d34d50b9f782d
Kicsi.T1 <totin.2citromail.hu@gmail.com>
2026-03-11 16:11:15 +0100

    The mockup of the prodctinfo component is done.

--- src/app/components/shop/product-info/product-info.ts
+++ src/app/components/shop/product-info/product-info.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
-
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {IconDefinition} from '@fortawesome/fontawesome-svg-core';
+import {faShareFromSquare,faCircleQuestion,faStar, faEye , faTruck, faSquare } from '@fortawesome/free-regular-svg-icons';
+import { faCcVisa, faCcMastercard, faCcAmex, faCcJcb, faCcDiscover, faCcDinersClub, faCcPaypal, } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-product-info',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './product-info.html',
-  styleUrl: './product-info.css',
+  styleUrl: './product-info.scss',
 })
 export class ProductInfo {
-
+  faStar: IconDefinition = faStar;
+  faEye: IconDefinition = faEye;
+  faCcVisa: IconDefinition = faCcVisa;
+  faCcMastercard: IconDefinition = faCcMastercard;
+  faCcAmex: IconDefinition = faCcAmex;
+  faCcJcb: IconDefinition = faCcJcb;
+  faCcDiscover: IconDefinition = faCcDiscover;
+  faCcDinersClub: IconDefinition = faCcDinersClub;
+  faCcPaypal: IconDefinition = faCcPaypal;
+  faTruck: IconDefinition = faTruck;
+  faSquare: IconDefinition = faSquare;
+  faCircleQuestion: IconDefinition = faCircleQuestion;
+  faShareFromSquare: IconDefinition = faShareFromSquare;
 }
