a0d526225fe8ab41e1c088cb1c21662fd63d8b30
Kicsi.T1 <totin.2citromail.hu@gmail.com>
2026-03-28 19:19:21 +0100

    In the Payment Input section, error messages now appear under the fields.

--- src/app/components/shop/purchasing-products/purchasing-products.ts
+++ src/app/components/shop/purchasing-products/purchasing-products.ts
@@ -22,17 +22,18 @@ export class PurchasingProducts {
     
     this.ContactForm = this.fb.group
     ({
-      country: ['', Validators.required],
-      email: ['', [Validators.email]],
-      FirstName:['',[Validators.minLength(3),Validators.maxLength(40)]],
-      LastName:['',[Validators.minLength(3),Validators.maxLength(40),Validators.pattern('^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ]+$')]],
-      Address:['',[Validators.maxLength(60),Validators.required]],
-      City:['',[Validators.maxLength(60),Validators.required]],
-      PostalCode:['',[Validators.minLength(6),Validators.maxLength(20), Validators.pattern('^[0-9]+$'),Validators.required]],
+      country:    ['', Validators.required],
+      email:      ['',[Validators.email]],
+      FirstName:  ['',[Validators.minLength(3),Validators.maxLength(40)]],
+      LastName:   ['',[Validators.minLength(3),Validators.maxLength(40),Validators.pattern('^[a-zA-ZáéíóöőúüűÁÉÍÓÖŐÚÜŰ]+$')]],
+      Address:    ['',[Validators.maxLength(60),Validators.required]],
+      City:       ['',[Validators.maxLength(60),Validators.required]],
+      PostalCode: ['',[Validators.minLength(6),Validators.maxLength(20), Validators.pattern('^[0-9]+$'),Validators.required]],
     });
 
     this.Payment=this.fb.group
     ({
+        Payment:['',[Validators.required]],
         CardNumber:['',[Validators.required,Validators.maxLength(16)]],
         ExpirationDate:['',[Validators.required,Validators.pattern('^(0[1-9]|1[0-2])\\/([0-9]{4})$')]],
         SecurityCode:['',[Validators.required,Validators.maxLength(3)]],
