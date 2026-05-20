f1439dc91a02717ee4208eb85c4a7ded4210649e
Kicsi.T1 <totin.2citromail.hu@gmail.com>
2026-05-20 18:38:30 +0200

    The filter can now be closed in phone view.

--- src/app/components/shop/product-layout-switcher/product-layout-switcher.ts
+++ src/app/components/shop/product-layout-switcher/product-layout-switcher.ts
@@ -1,15 +1,17 @@
-import { Component,Input } from '@angular/core';
+import { Component, Input, Output,EventEmitter } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { faAngleDown} from '@fortawesome/free-solid-svg-icons';
 import { COLLECTION } from '../../../models/product.model';
 import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-product-layout-switcher',
   imports: [FontAwesomeModule,FormsModule],
   templateUrl: './product-layout-switcher.html',
   styleUrl: './product-layout-switcher.scss',
 })
+
 export class ProductLayoutSwitcher {
 
   @Input() ProdCollectionIndex:number=-1;
--- src/app/components/shop/shop.ts
+++ src/app/components/shop/shop.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener  } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { faAngleUp,faAngleDown} from '@fortawesome/free-solid-svg-icons';
@@ -7,7 +7,7 @@ import { ProductLayoutSwitcher } from './product-layout-switcher/product-layout-
 import { ProductContent } from './product-content/product-content';
 import { Pagination } from './pagination/pagination';
 import { ProductModel } from '../../models/product.model';
-
+import { ElementRef } from '@angular/core';
 @Component({
   selector: 'app-Shop',
   imports: [FontAwesomeModule,ProductFilter,ProductLayoutSwitcher,ProductContent,Pagination],
@@ -32,5 +32,18 @@ export class Shop {
 
   faAngleUp:IconDefinition=faAngleUp;
   faAngleDown:IconDefinition=faAngleDown;
+  MouseOnPhoneFilter:boolean=true;
 
+  constructor(private elementRef: ElementRef) {}
+
+  @HostListener('document:click', ['$event'])
+  clickOutside(event: MouseEvent) 
+  {
+    const clickedInside = this.elementRef.nativeElement.querySelector('.PhoneFilter')?.contains(event.target);
+    if(!clickedInside) 
+    {
+      this.MouseOnPhoneFilter = false;
+      document.body.style.overflow = 'auto';
+    }
+  }
 }
