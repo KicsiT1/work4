4ce64e923b07ca2a65f1bf2d6a4e03cf3e9a18a2
Kicsi.T1 <totin.2citromail.hu@gmail.com>
2026-06-07 18:52:23 +0200

    You can now move forward and backward in pagination.

--- src/app/components/shop/pagination/pagination.ts
+++ src/app/components/shop/pagination/pagination.ts
@@ -12,17 +12,38 @@ export class Pagination
   @Input() productsLength!:number;
   PagesNumber:number=0;
   PagesArray:number[]=[];
+  PagignationStart:number=0;
+  PaginationEnd:number=3;
   ngOnChanges(changes: SimpleChanges) 
   {
     if (changes['productsLength']) 
     {
       this.productsLength = changes['productsLength'].currentValue;
       this.PagesNumber = this.productsLength % 9 === 0 ? Math.floor(this.productsLength / 9) : Math.floor(this.productsLength / 9) + 1;
-
+      this.PagesArray = [];
       for(let i=1;i<=this.PagesNumber;i++)
       {
         this.PagesArray.push(i);
+        console.log(this.PagesArray);
       }
+    }
+  }
+
+  Previous()
+  {
+    if(this.PagignationStart!==0)
+    {
+      this.PagignationStart-=1;
+      this.PaginationEnd-=1;
+    }
+  }
+
+  next()
+  {
+    if(this.PaginationEnd!==this.PagesNumber)
+    {
+      this.PagignationStart+=1;
+      this.PaginationEnd+=1;
     }
   }
 }
